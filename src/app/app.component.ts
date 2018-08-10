07f28e9f9cfb6a97c3f1b0521a185a370c52bdaf
Divya Elsa <divyaelsaaagnes@gmail.com>
2018-08-10 19:14:35 +0530

    resolve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,63 +24,63 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any;
-  admin:any;
-  color:any;
-  adminRoles:any;
+  admin: any;
+  color: any;
+  adminRoles: any;
   navCtrl: NavController
-  pages: Array<{title: string, component: any,icon:any,bg_color:any,color:any}>;
-  HOpages: Array<{title: string, component: any,icon:any,bg_color:any,color:any}>
-  LSpages:Array<{title: string, component: any,icon:any,bg_color:any,color:any}>
+  pages: Array<{ title: string, component: any, icon: any, bg_color: any, color: any }>;
+  HOpages: Array<{ title: string, component: any, icon: any, bg_color: any, color: any }>
+  LSpages: Array<{ title: string, component: any, icon: any, bg_color: any, color: any }>
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,public storage:StorageProvider,public splitPane:SplitpaneProvider,private daterangepickerOptions: DaterangepickerConfig,private app:App,private menuCtrl:MenuController) {
-    this.initializeApp();
-    this.admin={};
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public storage: StorageProvider, public splitPane: SplitpaneProvider, private daterangepickerOptions: DaterangepickerConfig, private app: App, private menuCtrl: MenuController) {
+    this.pages=[];
+    this.admin = {};
     this.navCtrl = app.getActiveNav();
-    this.storage.getItem('admin').then((data:any)=>{
+    this.storage.getItem('admin').then((data: any) => {
       console.log(data);
-      if(data!=null){
+      if (data != null) {
         //this.openPage(HomePage)
-        this.admin=data.admin;
+        this.admin = data.admin;
         console.log(this.admin);
         console.log(data);
-        this.adminRoles=data.authenticationDetails.roles;
-        this.rootPage=HomePage;
+        this.adminRoles = data.authenticationDetails.roles;
+        this.rootPage = HomePage;
         this.splitPane.setSplitPane(true);
         // set navigation based on admin roles
-        if(data.admin.rolesMap.HO){
+        if (data.admin.rolesMap.HO) {
           this.pages = [
-            { title: 'Dashboard', component: HomePage,icon:'home',bg_color:'secondary',color:'primary' },
-            { title: 'Orders', component: OrdersPage,icon:'cart',bg_color:'secondary',color:'primary' },
-            { title: 'Required Stock', component: InventoryPage,icon:'cube',bg_color:'secondary',color:'primary' },
-            { title: 'Catalog', component: CatalogPage,icon:'list',bg_color:'secondary',color:'primary' },
-            { title: 'Banners', component: PromotionsPage,icon:'list',bg_color:'secondary',color:'primary' }
-          ];  
-        }else if(data.admin.rolesMap.LS){
+            { title: 'Dashboard', component: HomePage, icon: 'home', bg_color: 'secondary', color: 'primary' },
+            { title: 'Orders', component: OrdersPage, icon: 'cart', bg_color: 'secondary', color: 'primary' },
+            { title: 'Required Stock', component: InventoryPage, icon: 'cube', bg_color: 'secondary', color: 'primary' },
+            { title: 'Catalog', component: CatalogPage, icon: 'list', bg_color: 'secondary', color: 'primary' },
+            { title: 'Banners', component: PromotionsPage, icon: 'list', bg_color: 'secondary', color: 'primary' }
+          ];
+        } else if (data.admin.rolesMap.LS) {
           this.pages = [
-            { title: 'Dashboard', component: HomePage,icon:'home',bg_color:'secondary',color:'primary' },
-            { title: 'Orders', component: LocalvendorOrdersPage,icon:'cart',bg_color:'secondary',color:'primary' },
-            { title: 'Required Stock', component: InventoryPage,icon:'cube',bg_color:'secondary',color:'primary' },
-            { title: 'Catalog', component: CatalogPage,icon:'list',bg_color:'secondary',color:'primary' },
-            { title: 'Apartments', component: ApartmentsPage,icon:'home',bg_color:'secondary',color:'primary' }
-          ]; 
-        }else{
-          // this.pages = [
-          //   { title: 'Dashboard', component: HomePage,icon:'home',bg_color:'secondary',color:'primary' },
-          //   { title: 'Orders', component: OrdersPage,icon:'cart',bg_color:'secondary',color:'primary' },
-          //   { title: 'Required Stock', component: InventoryPage,icon:'cube',bg_color:'secondary',color:'primary' }
-          // ];  
+            { title: 'Dashboard', component: HomePage, icon: 'home', bg_color: 'secondary', color: 'primary' },
+            { title: 'Orders', component: LocalvendorOrdersPage, icon: 'cart', bg_color: 'secondary', color: 'primary' },
+            { title: 'Required Stock', component: InventoryPage, icon: 'cube', bg_color: 'secondary', color: 'primary' },
+            { title: 'Catalog', component: CatalogPage, icon: 'list', bg_color: 'secondary', color: 'primary' },
+            { title: 'Apartments', component: ApartmentsPage, icon: 'home', bg_color: 'secondary', color: 'primary' }
+          ];
+        } else {
+          this.pages = [
+            { title: 'Dashboard', component: HomePage, icon: 'home', bg_color: 'secondary', color: 'primary' },
+            { title: 'Orders', component: OrdersPage, icon: 'cart', bg_color: 'secondary', color: 'primary' },
+            { title: 'Required Stock', component: InventoryPage, icon: 'cube', bg_color: 'secondary', color: 'primary' }
+          ];
         }
-              
-      }else{
+
+      } else {
         this.rootPage = LoginPage;
       }
-    },() => {
-        console.log('data');
-        this.rootPage = LoginPage;
-      })
+    }, () => {
+      console.log('data');
+      this.rootPage = LoginPage;
+    })
 
+    this.initializeApp();
 
-  
     // configuring date range
     this.daterangepickerOptions.settings = {
       locale: { format: 'YYYY-MM-DD' },
@@ -92,9 +92,10 @@ export class MyApp {
         'Last 30 Days': [moment().startOf('day').subtract(29, 'days'), moment().startOf('day').add(1, 'days').subtract(1, 'minute')],
         'This Month': [moment().startOf('day').startOf('month'), moment().startOf('day').add(1, 'day').subtract(1, 'minute')],
         'Last Month': [moment().startOf('day').subtract(1, 'month').startOf('month'), moment().startOf('day').subtract(1, 'month').endOf('month')]
-    }
+      }
 
-  };
+
+    };
 
   }
 
@@ -114,25 +115,23 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
     for (let p of this.pages) {
-    
-      if(p.title==page.title)
-      {
-        p.color='secondary';
+
+      if (p.title == page.title) {
+        p.color = 'secondary';
       }
-      else
-      {
-        p.color='primary';
+      else {
+        p.color = 'primary';
       }
-      
-      }
+
+    }
   }
 
 
 
-  logout(){
+  logout() {
     this.storage.clearItem();
     this.menuCtrl.close();
     this.app.getRootNav().setRoot(LoginPage)
-    
+
   }
 }
